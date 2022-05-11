a41c3a3c50f08d729b30fb665f6c85feb2e76d88
LeeMoonge <1441344762@qq.com>
2022-05-11 14:32:33 +0800

    初始化模块

---
+++ addons/ztest_estate/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/5/11 14:20
+# @Author : LeeMoonge
---
+++ addons/ztest_estate/__manifast__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/5/11 14:21
+# @Author : LeeMoonge
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': '房地产广告',
+    'version': '1.0',
+    'summary': 'real estate',
+    'description': """
+    Real Estate
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'application': True,
+}
