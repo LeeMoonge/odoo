1d047c7f362cb378c8735b98bac2164d3f0d3cf2
LeeMoonge <1441344762@qq.com>
2022-05-11 16:46:02 +0800

    debug

--- my_addons/estate/__manifest__.py
+++ my_addons/estate/__manifest__.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "estate",
+    'name': "房地产广告",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """real estate""",
 
     'description': """
-        Long description of module's purpose
+        Real Estate
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "LeeMoonge",
+    # 'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    # 'depends': ['base'],
+    'depends': [],
 
     # always loaded
     'data': [
@@ -32,4 +31,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True,
 }
--- my_addons/estate/models/models.py
+++ my_addons/estate/models/models.py
@@ -16,3 +16,25 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+from odoo import models, fields
+
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = '房地产广告模块'
+
+    name = fields.Char(string='name')
+    description = fields.Text(string='description')
+    postcode = fields.Char(string='postcode')
+    date_availability = fields.Date(string='date_availability')
+    expected_price = fields.Float(string='expected_price')
+    selling_price = fields.Float(string='selling_price')
+    bedrooms = fields.Integer(string='bedrooms')
+    living_area = fields.Integer(string='living_area')
+    facades = fields.Integer(string='facades')
+    garage = fields.Boolean(string='garage')
+    garden = fields.Boolean(string='garden')
+    garden_area = fields.Integer(string='garden_area')
+    garden_orientation = fields.Selection([('North', '北'), ('South', '南'), ('East', '东'), ('West', '西')],
+                                          string='garden_orientation')
